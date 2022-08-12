5adcfbb5d254e0cb59c500c62491d598f723149b
Oyatullo <oyatullohkhan3@gmail.com>
2022-08-12 08:48:22 +0500

    flagquiz

---
+++ app/src/main/java/com/oyatulloh/myfirstproject/models.kt
@@ -0,0 +1,11 @@
+package com.oyatulloh.myfirstproject
+
+class Flag {
+    var name:String? = null
+    var image:Int? = null
+
+    constructor(name: String?, image: Int?) {
+        this.name = name
+        this.image = image
+    }
+}
